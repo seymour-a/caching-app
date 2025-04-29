625e66b6daeaad046e839d948563dab3e2e727bf
SeymurAhmadzada <seymur.ahmadzada@gmail.com>
2025-04-30 01:13:27 +0400

    fix: - Bugs Fixed

--- src/main/java/com/seymurahmadzada/cachingapp/controller/ProductController.java
+++ src/main/java/com/seymurahmadzada/cachingapp/controller/ProductController.java
@@ -3,7 +3,10 @@ package com.seymurahmadzada.cachingapp.controller;
 import com.seymurahmadzada.cachingapp.model.entity.Product;
 import com.seymurahmadzada.cachingapp.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/v1/products")
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final ProductService productService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @GetMapping("/{id}")
     public Product getProduct(@PathVariable Long id) {
@@ -26,6 +30,24 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("test1");
+        product.setDescription("demo1");
+
+        redisTemplate.opsForValue().set("seymur", product, 1,TimeUnit.MINUTES);
+        return "test2";
+    }
+
+    @GetMapping("/get")
+    public Object get() {
+
+        Object seymur = redisTemplate.opsForValue().get("seymur");
+        return seymur;
     }
 
 }
